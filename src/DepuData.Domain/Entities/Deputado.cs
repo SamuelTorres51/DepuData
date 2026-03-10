f52705930e2258c4707f1c229e314238a07b26f6
SamuelTorres51 <samuel.torres28@hotmail.com>
2026-03-10 18:51:27 -0300

    feat: modelando as entidades do dominio

---
+++ src/DepuData.Domain/Entities/Deputado.cs
@@ -0,0 +1,10 @@
+﻿namespace DepuData.Domain.Entities;
+
+public class Deputado {
+    public int Id { get; private set; }
+    public string Nome { get; private set; } = string.Empty;
+    public string Partido { get; private set; } = string.Empty;
+    public string Estado { get; private set;  } = string.Empty;
+    public List<Despesas> Despesas { get; private set; } = [];
+
+}
---
+++ src/DepuData.Domain/Entities/Despesas.cs
@@ -0,0 +1,7 @@
+﻿namespace DepuData.Domain.Entities;
+
+public class Despesas {
+    public string Tipo { get; set; } = string.Empty;
+    public decimal Valor { get; set; }
+    public DateTime Data { get; set; }
+}
